2a9fbcbadb0cfaa62a53cf656e94c8350ad181e3
Matthias Ruf <Matthias@ruf-media.com>
2023-12-22 14:38:08 +0100

    Initial Commit

---
+++ rufmediaapp/rufmediaappApp.swift
@@ -0,0 +1,17 @@
+//
+//  rufmediaappApp.swift
+//  rufmediaapp
+//
+//  Created by Matthias Ruf on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct rufmediaappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
